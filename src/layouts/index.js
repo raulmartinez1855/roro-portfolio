0907029dc23efebb93c1063fd3066088e413cac3
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2019-01-29 19:46:23 -0500

    fix for favicon rev 1

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Footer from '../components/Footer'
 import '../assets/sass/index.scss'
-import favicon from '../../public/favicon.ico'
+import favicon from '../../favicon.ico'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -13,7 +13,7 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Portfolio' },
         { name: 'keywords', content: 'react, gatsby' },
       ]}
-      link={[{ rel: 'favicon', type: 'image/x-icon', href: `${favicon}` }]}
+      link={[{ rel: 'favicon', type: 'image/png', href: `${favicon}` }]}
     />
     <div>{children()}</div>
     <Footer />
