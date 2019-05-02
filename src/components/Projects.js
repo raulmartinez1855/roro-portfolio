e4411614bfd31f0d23dbbafabe490194472213c3
Raul Martinez <raulmartinez1855@gmail.com>
2019-05-02 06:30:39 -0400

    Added Idania's Listing Project
    added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,16 @@ class Projects extends Component {
       demo: 'https://tender-sinoussi-aac2c6.netlify.com/',
     },
     {
+      name: "Idania's Listings",
+      description: `Full Stack Application I created for scraping public notices. The client is not a developer and wanted a 
+        very simple way to grab new leads. I created a Backend API using Node, Express, and MongoDB. Then the 
+        Frontend Is all in React. The bulk of the work is in a daily webscraper that will grab the public notices in paragraph format,
+        then clean the data up and insert it into a database that will not allow duplicate records. The Frontend hits the Backend API
+         and transforms the JSON into CSV format.`,
+      github: 'https://github.com/raulmartinez1855/rim-realty-foreclosure-app',
+      demo: 'https://idanias-listings.netlify.com/',
+    },
+    {
       name: 'Catch of the Day',
       description:
         'First React Course I took was building this application. After completing the course and learning about more about React. I recreated the application from scratch applying the concepts I learned',
@@ -41,7 +51,7 @@ class Projects extends Component {
           {this.projects.map((o, i) => {
             const isLast = this.projects.length - 1 === i
             return (
-              <div v key={i}>
+              <div key={i}>
                 <div className="project">&#123;</div>
                 <div className="project property">
                   <strong>demo:</strong> <a href={o.demo}>{o.name}</a>,
