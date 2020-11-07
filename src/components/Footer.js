1aa22746b61a0fe916dc3afe210c0d7571e88ff4
Raul Martinez <raulmartinez1855@gmail.com>
2020-11-07 09:15:27 -0500

    update footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import GitHub from '../assets/images/GitHub-Mark-Light-32px.png'
-import LinkedIn from '../assets/images/linkedin.png'
 import Plane from '../assets/images/plane.svg'
 
 export default class Footer extends Component {
@@ -11,12 +9,19 @@ export default class Footer extends Component {
         <div className="images-container">
           <button>
             <a href="https://github.com/raulmartinez1855">
-              <img src={GitHub} alt="Raul's Github" />
+              <img
+                src="https://rawcdn.githack.com/raulmartinez1855/roro-portfolio/1b9b5648cca5679ec9495ca437c2a925c0fde4f7/src/assets/images/GitHub-Mark-Light-32px.png"
+                alt="Raul's Github"
+              />
             </a>
           </button>
           <button>
             <a href="https://www.linkedin.com/in/raulmartinez1/">
-              <img src={LinkedIn} alt="Raul's LinkedIn" id="size-fix" />
+              <img
+                src="https://rawcdn.githack.com/raulmartinez1855/roro-portfolio/1b9b5648cca5679ec9495ca437c2a925c0fde4f7/src/assets/images/In-White-34px-R.png"
+                alt="Raul's LinkedIn"
+                id="size-fix"
+              />
             </a>
           </button>
           <button>
